cd2120d61b0e3ccfe8b6ef1bad53a67ad0d3a674
Art Bender <artem.levchenko.vrp@gmail.com>
2022-02-15 09:46:59 +0300

    add error handling

--- src/main/java/com/artbender/web/controllers/NbaSimulateGameController.java
+++ src/main/java/com/artbender/web/controllers/NbaSimulateGameController.java
@@ -1,8 +1,10 @@
 package com.artbender.web.controllers;
 
 import com.artbender.model.dto.rest.*;
+import com.artbender.model.exceptions.NbaManagerException;
 import com.artbender.service.orchestrator.GameOrchestrator;
 import com.artbender.web.constants.GameAPI;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,37 +19,66 @@ public class NbaSimulateGameController {
 
     @PostMapping(value = GameAPI.MAKE_TURN)
     public ResponseEntity<GlobalGameResponse> execute() {
-        return ResponseEntity.accepted().body(gameOrchestrator.execute());
+        try {
+            return ResponseEntity.accepted().body(gameOrchestrator.execute());
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @PostMapping(value = GameAPI.INIT)
     public ResponseEntity<GlobalGameResponse> initGame(@RequestBody GlobalGameRequest teamGameRequest) {
-        return ResponseEntity.accepted().body(gameOrchestrator.initGame(teamGameRequest));
+        try {
+            return ResponseEntity.accepted().body(gameOrchestrator.initGame(teamGameRequest));
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = GameAPI.PLAYER_INFO)
     public ResponseEntity<IndividualPlayerCardResponse> playerInfo(@RequestParam Integer id) {
-        return ResponseEntity.accepted().body(gameOrchestrator.getPlayerCardInfo(id));
+        try {
+            return ResponseEntity.accepted().body(gameOrchestrator.getPlayerCardInfo(id));
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping(value = GameAPI.SAVE_PLAYER_SETTINGS)
     public ResponseEntity<GlobalGameResponse> savePlayerSettings(@RequestBody IndividualPlayerCardRequest individualPlayerCardRequest) {
-        return ResponseEntity.ok().body(gameOrchestrator.savePlayerSettings(individualPlayerCardRequest));
+        try {
+            return ResponseEntity.ok().body(gameOrchestrator.savePlayerSettings(individualPlayerCardRequest));
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value = GameAPI.COACH_INFO)
     public ResponseEntity<CoachSettingDTO> savePlayerSettings(@RequestParam Integer id) {
-        return ResponseEntity.accepted().body(gameOrchestrator.getCoachCardInfo(id));
+        try {
+            return ResponseEntity.accepted().body(gameOrchestrator.getCoachCardInfo(id));
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping(value = GameAPI.SAVE_COACH_SETTINGS)
     public ResponseEntity<Void> saveCoachSettings(@RequestBody CoachSettingDTO coachSettingDTO) {
-        gameOrchestrator.saveCoachSettings(coachSettingDTO);
-        return ResponseEntity.noContent().build();
+        try {
+            gameOrchestrator.saveCoachSettings(coachSettingDTO);
+            return ResponseEntity.noContent().build();
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping(value = GameAPI.TIMEOUT)
     public ResponseEntity<GlobalGameResponse> timeout(@RequestBody CoachSettingDTO coachSettingDTO) {
-        return ResponseEntity.accepted().body(gameOrchestrator.timeout(coachSettingDTO));
+        try {
+            return ResponseEntity.accepted().body(gameOrchestrator.timeout(coachSettingDTO));
+        } catch (NbaManagerException nbaManagerException) {
+            return new ResponseEntity(nbaManagerException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
